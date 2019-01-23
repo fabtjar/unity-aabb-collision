c31da43bb691481527a2cfd6731fbd0bce8c9a2f
Fabian Jarrett <fabianjarrett@gmail.com>
2019-01-23 08:41:14 +0000

    Initial commit

---
+++ Assets/ColliderBox.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class HitData
+{
+    public Vector2 overlap;
+}
+
+public class ColliderBox : MonoBehaviour
+{
+    public float width, height;
+
+    public HitData IsOverlapping(Vector2 offset, ColliderBox other)
+    {
+        if (
+            offset.x +
+            this.transform.position.x - this.width / 2 <
+            other.transform.position.x + other.width / 2 &&
+            offset.x + 
+            this.transform.position.x + this.width / 2 >
+            other.transform.position.x - other.width / 2 &&
+            offset.y +
+            this.transform.position.y - this.height / 2 <
+            other.transform.position.y + other.height / 2 &&
+            offset.y +
+            this.transform.position.y + this.height / 2 >
+            other.transform.position.y - other.height / 2
+        )
+        {
+            HitData hit = new HitData
+            {
+                overlap = new Vector2(
+                    (this.width / 2 + other.width / 2) -
+                    Mathf.Abs((this.transform.position.x + offset.x) - other.transform.position.x),
+                    (this.height / 2 + other.height / 2) -
+                    Mathf.Abs((this.transform.position.y + offset.y) - other.transform.position.y)
+                )
+            };
+            return hit;
+        }
+        return null;
+    }
+}
+
---
+++ Assets/Player.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float moveSpeed = 4f;
+    Vector2 velocity;
+
+    ColliderBox[] walls;
+    ColliderBox colliderBox;
+
+    SpriteRenderer sprite;
+
+    void Start()
+    {
+        colliderBox = GetComponent<ColliderBox>();
+
+        var wallObjects = GameObject.FindGameObjectsWithTag("Wall");
+        walls = new ColliderBox[wallObjects.Length];
+        for (int i = 0; i < walls.Length; i++)
+            walls[i] = wallObjects[i].GetComponent<ColliderBox>();
+    }
+
+    void Update()
+    {
+        velocity.x = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        velocity.y = Input.GetAxisRaw("Vertical") * moveSpeed;
+
+        Vector2 movement = velocity * Time.deltaTime;
+
+        foreach (var wall in walls)
+        {
+            HitData hit = colliderBox.IsOverlapping(new Vector2(movement.x, 0), wall);
+
+            if (hit != null)
+            {
+                movement.x -= Mathf.Sign(movement.x) * hit.overlap.x;
+            }
+        }
+
+        foreach (var wall in walls)
+        {
+            HitData hit = colliderBox.IsOverlapping(new Vector2(movement.x, movement.y), wall);
+
+            if (hit != null)
+            {
+                movement.y -= Mathf.Sign(movement.y) * hit.overlap.y;
+            }
+        }
+
+        transform.Translate(movement);
+    }
+}
